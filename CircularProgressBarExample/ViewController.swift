9c75d39df839a9bbe34808a058d2d2463a03a211
park kyung suk <boardguy1024@gmail.com>
2018-04-25 11:59:14 +0900

    trackLayerを追加

--- CircularProgressBarExample/ViewController.swift
+++ CircularProgressBarExample/ViewController.swift
@@ -14,17 +14,28 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    
         let center = view.center
-        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: true)
-        shapeLayer.path = circularPath.cgPath
+        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi / 2, endAngle: CGFloat.pi * 2, clockwise: true)
+        
+        // trackLayer
+        let trackLayer = CAShapeLayer()
+        trackLayer.path = circularPath.cgPath
+        trackLayer.strokeColor = UIColor.gray.cgColor
+        trackLayer.lineWidth = 10
+        trackLayer.fillColor = UIColor.clear.cgColor
+        trackLayer.lineCap = kCALineCapRound
+        view.layer.addSublayer(trackLayer)
         
         // line
+        shapeLayer.path = circularPath.cgPath
         shapeLayer.strokeColor = UIColor.red.cgColor
         shapeLayer.lineWidth = 10
-        
-        //0にすることでendに到達した時点でstrokeは0になる
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        //strokeEndのdefaultは1なのでstrokeが全部描画される
+        //0は当然描画なし、0.5は半分描画される
         shapeLayer.strokeEnd = 0
+        shapeLayer.lineCap = kCALineCapRound
         
         view.layer.addSublayer(shapeLayer)
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
@@ -36,6 +47,14 @@ class ViewController: UIViewController {
         //shapeLayerでStartとEndを基準にインタバルを設定することでAnimationするためのクラス
         let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
         basicAnimation.toValue = 1
+        
+        //animation duration
+        basicAnimation.duration = 2
+    
+        //animationが終わっても描いたstrokeを維持する
+        basicAnimation.fillMode = kCAFillModeForwards
+        basicAnimation.isRemovedOnCompletion = false
+        
         shapeLayer.add(basicAnimation, forKey: "some")
     }
 
