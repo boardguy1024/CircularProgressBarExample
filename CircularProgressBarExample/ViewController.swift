44f51511f3937e1189b02aa5f4103b681cd594a3
park kyung suk <boardguy1024@gmail.com>
2018-04-25 11:21:53 +0900

    円をタップしてpathがアニメーションで円を描くテスト

--- CircularProgressBarExample/ViewController.swift
+++ CircularProgressBarExample/ViewController.swift
@@ -9,17 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let shapeLayer = CAShapeLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let center = view.center
+        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: true)
+        shapeLayer.path = circularPath.cgPath
+        
+        // line
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        shapeLayer.lineWidth = 10
+        
+        //0にすることでendに到達した時点でstrokeは0になる
+        shapeLayer.strokeEnd = 0
+        
+        view.layer.addSublayer(shapeLayer)
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc private func handleTap() {
+        print("Circle tapped!!")
+        
+        //shapeLayerでStartとEndを基準にインタバルを設定することでAnimationするためのクラス
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimation.toValue = 1
+        shapeLayer.add(basicAnimation, forKey: "some")
     }
-
 
 }
 
